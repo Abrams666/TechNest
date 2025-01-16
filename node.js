8ffcbf46b24ec0cf8a230fa93d9dd6b15e5fccaf
Abrams666 <yuanchianchen@gmail.com>
2025-01-16 14:37:38 +0800

    0.3.2

--- node.js
+++ node.js
@@ -6,11 +6,12 @@ const fs = require("fs");
 //router
 const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
-    console.log(parsedUrl);
+    //console.log(parsedUrl);
     //homepage
     if (parsedUrl.pathname === "/" || parsedUrl.pathname === "/home") {
         fs.readFile("./Template/HomePage_Temp.html", (err, data) => {
-            const output = data.toString().replace("{% CSSFILE %}", `Template\\css.css`);
+            let output = data.toString().replace("{% CSS_FILE %}", "Template/css.css");
+            //output = output.replace("{%FACE_IMG%}", "/img/face-img.jpg");
 
             //console.log(output);
             res.setHeader("Content-Type", "text/html");
@@ -39,9 +40,16 @@ const server = http.createServer((req, res) => {
             res.setHeader("Content-Type", "text/css");
             res.end(output);
         });
+    } else if (parsedUrl.pathname[0] + parsedUrl.pathname[1] + parsedUrl.pathname[2] + parsedUrl.pathname[3] === "/img") {
+        fs.readFile(`./Template/Server_Picture/${parsedUrl.pathname[5] + parsedUrl.pathname[6] + parsedUrl.pathname[7]}.jpg`, (err, data) => {
+            console.log(`./Template/Server_Picture/${parsedUrl.pathname[5] + parsedUrl.pathname[6] + parsedUrl.pathname[7]}.jpg`);
+            let output = data;
+            res.setHeader("Content-Type", "image/jpeg");
+            res.end(output);
+        });
     } else {
         fs.readFile("./Template/404.html", (err, data) => {
-            const output = data.toString().replace("{% CSSFILE %}", `Template\\css.css`);
+            const output = data.toString().replace("{% CSS_FILE %}", `Template/css.css`);
 
             //console.log(output);
             res.setHeader("Content-Type", "text/html");
