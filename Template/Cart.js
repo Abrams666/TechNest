6a7f5fad01fb75aeb7bd58d6a505dfda31eb0bb3
Abrams <yuanchianchen@gmail.com>
2025-02-20 10:40:33 +0800

    0.21.0

--- Template/Cart.js
+++ Template/Cart.js
@@ -1,40 +1,40 @@
 const app = Vue.createApp({
-    data() {
-        return {};
-    },
-    methods: {
-        add(id) {
-            fetch(`/additemdata/add/${id}`, {
-                method: "POST",
-            }).then((res) => {
-                if (res.status === 201) {
-                    window.location.href = "/cart";
-                }
-            });
-        },
-        reduce(id) {
-            fetch(`/additemdata/reduce/${id}`, {
-                method: "POST",
-            }).then((res) => {
-                if (res.status === 201) {
-                    window.location.href = "/cart";
-                }
-            });
-        },
-        ddelete(id) {
-            console.log("delete");
+	data() {
+		return {};
+	},
+	methods: {
+		add(id) {
+			fetch(`/additemdata/add/${id}`, {
+				method: "POST",
+			}).then((res) => {
+				if (res.status === 201) {
+					window.location.href = "/cart";
+				}
+			});
+		},
+		reduce(id) {
+			fetch(`/additemdata/reduce/${id}`, {
+				method: "POST",
+			}).then((res) => {
+				if (res.status === 201) {
+					window.location.href = "/cart";
+				}
+			});
+		},
+		ddelete(id) {
+			console.log("delete");
 
-            fetch(`/additemdata/delete/${id}`, {
-                method: "POST",
-            }).then((res) => {
-                if (res.status === 201) {
-                    window.location.href = "/cart";
-                }
-            });
-        },
-        pay() {
-            window.location.href = "/pay";
-        },
-    },
+			fetch(`/additemdata/delete/${id}`, {
+				method: "POST",
+			}).then((res) => {
+				if (res.status === 201) {
+					window.location.href = "/cart";
+				}
+			});
+		},
+		pay() {
+			window.location.href = "/pay";
+		},
+	},
 });
 app.mount("#cart_formId");
